42ee673cc1cdffc7b3e6137642b502dd24495b19
rithy <eangrithy007@gmail.com>
2025-06-14 15:05:32 +0700

    feat(user): clear default userDetails in user store for improved state management

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -27,42 +27,7 @@ export const useUserStore = defineStore('user', {
     storage: sessionStorage,
   },
   state: () => ({
-    userDetails: {
-      displayName: 'Jack Handy',
-      authorities: [
-        {
-          authority: 'admin',
-        },
-      ],
-      roles: [
-        'admin',
-      ],
-      user: {
-        avatarName: 'avatar-20250114101539224.png',
-        avatarPath: '/Users/jie/Documents/work/me/admin/eladmin-mp/eladmin/~/avatar/avatar-20250114101539224.png',
-        dept: {
-          id: 2,
-          name: '研发部',
-        },
-        email: '201507802@qq.com',
-        enabled: true,
-        gender: '男',
-        id: 1,
-        nickName: '管理员',
-        password: '$2a$10$Egp1/gvFlt7zhlXVfEFw4OfWQCGPw0ClmMcc6FjTnvXNRVf9zdMRa',
-        phone: '18888888888',
-        pwdResetTime: '2020-05-03 08:38:31',
-        roles: [
-          {
-            dataScope: '全部',
-            id: 1,
-            level: 1,
-            name: '超级管理员',
-          },
-        ],
-        username: 'admin',
-      },
-    },
+    userDetails: {},
   }),
   actions: {
     setUserDetails(details: UserDetails) {
